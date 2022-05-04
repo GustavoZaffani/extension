0ffb2d92c2d64e393dbe80c13af8562964defdfd
Gustavo Henrique Zaffani <g_zaffani@hotmail.com>
2022-05-03 21:44:49 -0300

    Ajustes extensão

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
-var cardEl = document.getElementById('card');
+const API_URL = "https://raw.githubusercontent.com/Loscou/extension-links/main/extension.json";
 
-this.getData().then(function (data) {
-    this.buildCardLinks(data);
+document.addEventListener('DOMContentLoaded', function () {
+    cardEl = document.getElementById('container-item');
+    loaderEl = document.getElementById('loader');
+
+    showLoader(true);
+
+    getData().then((data) => {
+        buildCardLinks(data);
+        showLoader(false);
+    });
 });
 
-function getData() {
-    return new Promise(resolve => {
-        fetch("https://raw.githubusercontent.com/GustavoZaffani/projetoClient/master/package-lock.json")
-            .then(function (res) {
-                return res.json();
-            }).then(function (data) {
-            resolve(data);
+async function getData() {
+    return await fetch(API_URL)
+        .then((res) => {
+            return res.json();
+        }).then((data) => {
+            return data;
         });
-
-    })
 }
 
 function buildCardLinks(data) {
-    console.log(data);
+    if (!data?.length) return;
+
+    const cardLinkList = `
+        <ul class="container-items">
+            ${data.map(value => buildItem(value)).join(" ")}
+        </ul>
+    `;
+
+    cardEl.innerHTML = cardLinkList;
 }
 
+function buildItem(data) {
+    return `
+        <li class="item-list">
+            <a class="item" href="${data.link}" target="_blank">
+                <span class="title">${data.title}</span>
+                <p class="author">by ${data.author}</p>
+            </a>
+        </li>
+    `;
+}
+
+function showLoader(show) {
+    if (show) {
+        loaderEl.classList.remove('invisible');
+        cardEl.classList.add('invisible');
+    } else {
+        loaderEl.classList.add('invisible');
+        cardEl.classList.remove('invisible');
+    }
+}
